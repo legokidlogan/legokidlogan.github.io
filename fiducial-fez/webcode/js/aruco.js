77489389f8733416d21737af87364c54b1edc860
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-11-29 11:03:29 -0700

    Translate English to Zuish

--- fiducial-fez/webcode/js/aruco.js
+++ fiducial-fez/webcode/js/aruco.js
@@ -66,6 +66,37 @@ var allSymbolBits = [
   [ [0,0,1,1,0], [0,0,1,0,0], [0,1,0,1,0], [0,1,0,0,0], [0,0,0,1,0] ], // 27 cornerBR
 ];
 
+var letterToSymbolID = {
+  "A": 0,
+  "B": 1,
+  "C": 2,
+  "D": 3,
+  "E": 4,
+  "F": 5,
+  "G": 6,
+  "H": 7,
+  "I": 8,
+  "J": 9,
+  "K": 10, "Q": 10,
+  "L": 11,
+  "M": 12,
+  "N": 13,
+  "O": 14,
+  "P": 15,
+  "R": 16,
+  "S": 17,
+  "T": 18,
+  "U": 19, "V": 19,
+  "W": 20,
+  "X": 21,
+  "Y": 22,
+  "Z": 23,
+  "cornerTL": 24,
+  "cornerTR": 25,
+  "cornerBL": 26,
+  "cornerBR": 27,
+};
+
 AR.Marker = function(id, corners, colorState=0) {
   var sumX = 0, sumY = 0;
   var maxX = 0, maxY = 0, minX = 100000, minY = 100000;
